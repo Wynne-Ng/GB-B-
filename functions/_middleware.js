376990ed53d199cee995a97f832d43daceef6a92
Wynne-Ng <konstancewu@gmail.com>
2026-07-27 10:40:07 +0800

    Update _middleware.js
    export async function onRequest(context) {
    
      const { request, env } = context;
    
      const auth = request.headers.get("Authorization");
    
      const valid =
        "Basic " + btoa(`${env.SITE_USER}:${env.SITE_PASSWORD}`);
    
      if (auth !== valid) {
        return new Response("Login Required", {
          status: 401,
          headers: {
            "WWW-Authenticate": 'Basic realm="Private Tool"',
          },
        });
      }
    
      return context.next();
    }

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,1 +1,20 @@
+export async function onRequest(context) {
 
+  const { request, env } = context;
+
+  const auth = request.headers.get("Authorization");
+
+  const valid =
+    "Basic " + btoa(`${env.SITE_USER}:${env.SITE_PASSWORD}`);
+
+  if (auth !== valid) {
+    return new Response("Login Required", {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Private Tool"',
+      },
+    });
+  }
+
+  return context.next();
+}
