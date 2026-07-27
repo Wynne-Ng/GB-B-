f50f994c92209006d0731e73c996567d437895a6
Wynne-Ng <konstancewu@gmail.com>
2026-07-27 10:26:07 +0800

    Update _worker.js
    export default {
      async fetch(request, env) {
    
        const authorization = request.headers.get("Authorization");
    
        const expectedAuthorization =
          `Basic ${btoa(`${env.SITE_USER}:${env.SITE_PASSWORD}`)}`;
    
        if (authorization !== expectedAuthorization) {
          return new Response("Authorization required.", {
            status: 401,
            headers: {
              "WWW-Authenticate": 'Basic realm="Private Tool"',
            },
          });
        }
    
        return await env.ASSETS.fetch(request);
      },
    };

--- _worker.js
+++ _worker.js
@@ -1,1 +1,21 @@
 
+export default {
+  async fetch(request, env) {
+
+    const authorization = request.headers.get("Authorization");
+
+    const expectedAuthorization =
+      `Basic ${btoa(`${env.SITE_USER}:${env.SITE_PASSWORD}`)}`;
+
+    if (authorization !== expectedAuthorization) {
+      return new Response("Authorization required.", {
+        status: 401,
+        headers: {
+          "WWW-Authenticate": 'Basic realm="Private Tool"',
+        },
+      });
+    }
+
+    return await env.ASSETS.fetch(request);
+  },
+};
