cefdb017d9f5bb0c1d6f7e03f5d7fc479fbeefb8
Wynne-Ng <konstancewu@gmail.com>
2026-07-27 11:05:31 +0800

    Update _middleware.js
    export async function onRequest(context) {
    
      const { request, env } = context;
    
      const authorization = request.headers.get("Authorization");
    
      const username = env.SITE_USER;
      const password = env.SITE_PASSWORD;
    
      const expected = `Basic ${btoa(
        username + ":" + password
      )}`;
    
      if (authorization !== expected) {
        return new Response("Authorization required.", {
          status: 401,
          headers: {
            "WWW-Authenticate": 'Basic realm="Private Tool"',
            "Cache-Control": "no-store"
          },
        });
      }
    
      return context.next();
    }

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,15 +1,25 @@
 export async function onRequest(context) {
 
-  return new Response(
-    JSON.stringify({
-      user: context.env.SITE_USER,
-      pass: context.env.SITE_PASSWORD
-    }),
-    {
-      headers:{
-        "Content-Type":"application/json"
-      }
-    }
-  );
+  const { request, env } = context;
 
+  const authorization = request.headers.get("Authorization");
+
+  const username = env.SITE_USER;
+  const password = env.SITE_PASSWORD;
+
+  const expected = `Basic ${btoa(
+    username + ":" + password
+  )}`;
+
+  if (authorization !== expected) {
+    return new Response("Authorization required.", {
+      status: 401,
+      headers: {
+        "WWW-Authenticate": 'Basic realm="Private Tool"',
+        "Cache-Control": "no-store"
+      },
+    });
+  }
+
+  return context.next();
 }
