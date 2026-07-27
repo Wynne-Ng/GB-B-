32cd13937658c8f65094e39596bf428ccf9f59e5
Wynne-Ng <konstancewu@gmail.com>
2026-07-27 11:04:04 +0800

    Update _middleware.js
    export async function onRequest(context) {
    
      return new Response(
        JSON.stringify({
          user: context.env.SITE_USER,
          pass: context.env.SITE_PASSWORD
        }),
        {
          headers:{
            "Content-Type":"application/json"
          }
        }
      );
    
    }

--- functions/_middleware.js
+++ functions/_middleware.js
@@ -1,22 +1,15 @@
 export async function onRequest(context) {
 
-  const { request, env } = context;
+  return new Response(
+    JSON.stringify({
+      user: context.env.SITE_USER,
+      pass: context.env.SITE_PASSWORD
+    }),
+    {
+      headers:{
+        "Content-Type":"application/json"
+      }
+    }
+  );
 
-  const authorization = request.headers.get("Authorization");
-
-  const expected =
-    "Basic " + btoa(`${env.SITE_USER}:${env.SITE_PASSWORD}`);
-
-  if (authorization !== expected) {
-    return new Response("Authorization required.", {
-      status: 401,
-      headers: {
-        "WWW-Authenticate": 'Basic realm="Private Tool"',
-        "Cache-Control": "no-store"
-      },
-    });
-  }
-
-  return context.next();
 }
-// redeploy
